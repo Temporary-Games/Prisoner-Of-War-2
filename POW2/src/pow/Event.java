9306cdf17beec56f42cbb5e7c6451260fee63056
asc2 <asc2@10.9.34.54>
2014-12-03 13:25:23 -0600

    Event and Main

--- POW2/src/pow/Event.java
+++ POW2/src/pow/Event.java
@@ -6,70 +6,94 @@ import java.util.Scanner;
 
 public class Event {
 
-	//Names the card, such as "You find a chocolate bar!"
+	// Names the card, such as "You find a chocolate bar!"
 	String title;
-	
-	/* 
-	 * How it affects the overall prison-guard anger.
-	 * Can be positive or negative. Higher is angrier.
-	 * 
-	 */
-	int guardAggression;
-	
+
 	/*
-	 * If this is true, the event will display the title and
-	 * instantly free a prisoner.  The card will not be added
-	 * to the Player's inventory.
-	 * 
+	 * How it affects the overall prison-guard anger. Can be positive or
+	 * negative. Higher is angrier.
+	 */
+	public static int guardAggression;
+
+	/*
+	 * If this is true, the event will display the title and instantly free a
+	 * prisoner. The card will not be added to the Player's inventory.
 	 */
 	boolean freesAPrisoner;
-	
+
 	/*
-	 * If this is true, the card can be stored in your inventory
-	 * and used when a "bribable" card comes up.  (E.g. "Chocolate Bar"
-	 * would be a bribe and could be used on an "Aggressive Guard" card
-	 * which would be "bribable")
-	 * 
+	 * Number of Prisoners
 	 */
+	public int prisoners = 100;
 
 	int inventoryPosition = 0;
-	
+
 	public Event() throws FileNotFoundException {
-		//event reads in an event from "events.txt"
+		// event reads in an event from "events.txt"
 		Scanner event = new Scanner(new File("data/events.txt"));
-		//Test call of the event.txt file
-		int x = (int)(Math.random()*3);
-		for(int i = 0; i< x; i++){
+		// Test call of the event.txt file
+		int x = (int) (Math.random() * 3);
+		for (int i = 0; i < x; i++) {
 			event.nextLine();
 		}
-		
+
 		inventoryPosition = event.nextInt();
-		
+
 		title = event.next().replace('_', ' ');
 
-		guardAggression = event.nextInt();
+		guardAggression += event.nextInt();
 
-		
 		boolean freesAPrisoner = event.nextBoolean();
 
-		
-		
 	}
-	
-	public String getTitle(){
+
+	public String getTitle() {
 		return title;
 	}
-	public int getGuardAggression(){
-		return guardAggression;
-	}
-	public boolean getFreesAPrisoner(){
+
+	public boolean getFreesAPrisoner() {
 		return freesAPrisoner;
 	}
-	public int getInventoryPosition(){
+
+	public int getInventoryPosition() {
 		return inventoryPosition;
 	}
 	
+	public int getGuardAggression(){
+		return guardAggression;
+	}
+
+	
+	//unfinished method!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	public void killSavePrisoners() {
+		System.out.println("Guard Aggression: " + guardAggression);
+		int save = 0;
+		int kill = 0;
+		// Kill Prisoners
+		if (guardAggression >= 10) {
+			int count = 0;
+			for (guardAggression -= 10; guardAggression == 0; guardAggression -= 10) {
+				prisoners -= 1;
+				kill++;
+			}
+		}
+
+		// Save Prisoners
+		if (guardAggression <= -10) {
+			int count = 0;
+			for (guardAggression += 10; guardAggression == 0; guardAggression += 10) {
+				prisoners += 1;
+				save++;
+			}
+		}
+		
+		System.out.println(save + " prisoners were saved.");
+		System.out.println(kill + " prisoners were killed.");
+		
+	}
+
+	public int getNumPrisoners() {
+		return prisoners;
+	}
+
 }
-
-
-
--- POW2/src/pow/Main.java
+++ POW2/src/pow/Main.java
@@ -8,7 +8,6 @@ public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		int rounds = 30;
-		int guardAgro = 0;
 		int savedPrisoners = 0;
 		Inventory playerInv = new Inventory(0, 0, 0, 0, 0);
 		for (int currentRound = 1; currentRound <= rounds; currentRound++) {
@@ -18,28 +17,24 @@ public class Main {
 				System.out.println("Round: " + currentRound);
 				System.out.println("Freed Prisoners: " + savedPrisoners);
 				System.out.println(stuff.getTitle());
-				if(stuff.getInventoryPosition() != 0)
+				if (stuff.getInventoryPosition() != 0)
 					playerInv.addItem(stuff.getInventoryPosition());
-				if(stuff.getFreesAPrisoner() == true)
-				{
+				if (stuff.getFreesAPrisoner() == true) {
 					savedPrisoners++;
 					System.out.println("A prisoner was saved.");
 				}
-				guardAgro += stuff.getGuardAggression();
-				if(guardAgro < 0)
-					guardAgro = 0;
-				System.out.println("Guard Agression: " + guardAgro);
-				if (guardAgro >= 10) {
-					guardAgro = 0;
-					savedPrisoners--;
-					System.out.println("A prisoner was killed by the guards.");
-				}
-				
-				System.out.println("Inventory :: " + playerInv.toString());
-				
-				
-				System.out.println("Do you want to go to the next turn?(y/n)");
 
+				//System.out.println("Inventory :: " + playerInv.toString());
+
+				// Number of prisoners
+				System.out.println("Number of prisoners still alive: "
+						+ stuff.getNumPrisoners());
+
+				// Kill or Save Prisoners
+				stuff.killSavePrisoners();
+
+				// Do you want to go to the next turn?
+				System.out.println("Do you want to go to the next turn?(y/n)");
 
 			} while (!(scan.nextLine().equalsIgnoreCase("y")));
 
