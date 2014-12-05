e4437e79a2208b670da3bc3245d511a44e5ef873
asc2 <asc2@10.9.30.1>
2014-12-05 13:20:19 -0600

    OI

--- POW2/src/pow/Inventory.java
+++ POW2/src/pow/Inventory.java
@@ -5,6 +5,56 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Inventory {
+	
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	int e = 0;
+	
+	// declare scanner
+	Scanner scan;
+
+	public Inventory() {
+
+	}
+
+	// inventory constructor
+	public Inventory(int a1, int a2, int a3, int a4, int a5)
+			throws FileNotFoundException {
+
+		a = a1;
+		b = a2;
+		c = a3;
+		d = a4;
+		e = a5;
+		// set all the inventory slots empty
+		
+
+		// read in the file of events
+		scan = new Scanner(new File("data/events.txt"));
+	}
+	
+	public String toString(int q){
+		switch(q){
+		case 0: return "-EMPTY-";
+		case 1: return "Chocolate Bar";
+		case 2: return "Cigar Box";
+		}
+		return "ERROR";
+	}
+}
+
+
+
+
+/*package pow;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Inventory {
 	// inventory array instantiation
 	public int[] inventory = new int[5];
 
@@ -32,7 +82,7 @@ public class Inventory {
 
 	// adds item to empty space
 	public void addItem(int item) {
-		for (int x = 0; x < 4; x++) {
+		for (int x = 0; x < 5; x++) {
 			if (inventory[x] == 0) {
 				inventory[x] = item;
 				break;
@@ -76,7 +126,7 @@ public class Inventory {
 	public String toString() {
 		String output = "";
 		int check = 0;
-		inventory[0] = 1;
+		//inventory[0] = 1;  //Essential Starting Pack
 		for (int x = 0; x < inventory.length; x++) {
 			if (inventory[x] == 0)
 				output += "-EMPTY- ";
@@ -89,29 +139,12 @@ public class Inventory {
 						String ip = scan.next().replace('_', ' ');
 						ip = ip.substring(11, ip.length()-1);
 						output+=("-"+ip+"- ");
+						System.out.println(output);
 						
 					}
 					//System.out.println("IN:" + check);
 				}
 				//if(check == -1){
 					//break;
-				//}
-				if (check != -1)
-					scan.nextLine();
-				
-				//System.out
-						//.println("CONFIRMATION_CONTROL_STATION_ALPHA_PROTOCOL_6");
-
-			} while (check != -1);
-			
-		}
-		try {
-			scan = new Scanner(new File("data/events.txt"));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		//System.out.println("Output");
-		return output;
-	}
-}
+			*
+}*/
